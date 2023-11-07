d1dbbff6f0f872ecc46414c1049f4e144ded8dff
fishdulyapath <fishphatuna@gmail.com>
2023-11-07 16:24:03 +0700

    xx

--- android/app/src/main/kotlin/com/example/dedeowner/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dedeowner/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.smlsoft.dedeowner
 
 import io.flutter.embedding.android.FlutterActivity
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
-
 
 class MainActivity: FlutterActivity() {
-        AppCenter.start(getApplication(),"5ff2b340-ec58-4b85-aa90-1f15dffa5d25",
-            Analytics.class, Crashes.class);
 }
