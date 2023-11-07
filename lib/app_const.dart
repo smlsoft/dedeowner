f2a90c7438fd7081ac06dec26ddbeaa9b4a1b3cf
fishdulyapath <fishphatuna@gmail.com>
2023-11-07 16:03:40 +0700

    update

--- lib/app_const.dart
+++ lib/app_const.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const String servicePrdApi = 'https://api.dedepos.com';
   static const String serviceDevApi = 'https://api.dev.dedepos.com';
-  static const String reportDevApi = 'http://api.dev.dedepos.com/apireport';
+  static const String reportDevApi = 'http://192.168.2.67:3345/apireport';
   static const String reportPrdApi = 'http://api.dedepos.com/apireport';
   //static const String serviceApi = 'http://192.168.2.209';
   //static const String reportDevApi = 'http://192.168.1.11:3345/apireport';
